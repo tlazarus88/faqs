deb6ab624f8f19f00e51015b2de870bdd97bad41
tlazarus88 <trevor.das.lazarus@gmail.com>
2016-02-16 09:56:12 -0500

    JS Toggle
    Toggle on/off added

--- js/index.js
+++ js/index.js
@@ -4,6 +4,6 @@ $('#answer1').hide();
 $('#answer2').hide();
 $('#answer3').hide();
 
-$('#question1').on('click', function (){$('#answer1').slideDown();});
-$('#question2').on('click', function (){$('#answer2').slideDown();});
-$('#question3').on('click', function (){$('#answer3').slideDown();});
+$('#question1').on('click', function (){$('#answer1').slideToggle();});
+$('#question2').on('click', function (){$('#answer2').slideToggle();});
+$('#question3').on('click', function (){$('#answer3').slideToggle();});
